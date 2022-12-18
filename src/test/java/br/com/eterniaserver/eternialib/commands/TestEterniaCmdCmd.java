a88461313734bef4074fc4765ec95ca1c9785862
Yuri Nogueira <yurinogueira@id.uff.br>
2022-12-18 20:18:04 -0300

    test: adjust unit test

--- src/test/java/br/com/eterniaserver/eternialib/commands/TestEterniaCmdCmd.java
+++ src/test/java/br/com/eterniaserver/eternialib/commands/TestEterniaCmdCmd.java
@@ -76,26 +76,26 @@ public class TestEterniaCmdCmd {
     }
 
     @Test
-    void testEterniaReloadWarningAdviceConfigWithWrongCheck() {
+    void testEterniaReloadGenericWithCheck() {
         CommandSender sender = Mockito.mock(CommandSender.class);
         EterniaLib plugin = Mockito.mock(EterniaLib.class);
         CoreCfg coreCfg = Mockito.mock(CoreCfg.class);
         Component component = Mockito.mock(Component.class);
 
-        String entry = "eterniatest_config:d";
+        String entry = "eterniatest_config:t";
         String configString = "eterniatest_config";
         EterniaCmd eterniaCmd = new EterniaCmd(plugin);
 
-        Mockito.when(coreCfg.category()).thenReturn(ConfigurationCategory.WARNING_ADVICE);
+        Mockito.when(coreCfg.category()).thenReturn(ConfigurationCategory.GENERIC);
         Mockito.when(plugin.getConfiguration(configString)).thenReturn(coreCfg);
-        Mockito.when(plugin.getComponentMessage(Messages.CONFIG_ADVICE, true, configString)).thenReturn(component);
+        Mockito.when(plugin.getComponentMessage(Messages.CONFIG_RELOADED, true, configString)).thenReturn(component);
 
         eterniaCmd.onReload(sender, entry);
 
-        Mockito.verify(plugin, Mockito.times(1)).getComponentMessage(Messages.CONFIG_ADVICE, true, configString);
+        Mockito.verify(plugin, Mockito.times(1)).getComponentMessage(Messages.CONFIG_RELOADED, true, configString);
         Mockito.verify(sender, Mockito.times(1)).sendMessage(component);
-        Mockito.verify(coreCfg, Mockito.times(0)).executeConfig();
-        Mockito.verify(coreCfg, Mockito.times(0)).executeCritical();
+        Mockito.verify(coreCfg, Mockito.times(1)).executeConfig();
+        Mockito.verify(coreCfg, Mockito.times(1)).executeCritical();
     }
 
     @Test
